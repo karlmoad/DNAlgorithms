6a0246ff0de14aa67d394d3ba13aa924d2182ba8
Moad, Karl <karl.moad@gmail.com>
2018-02-19 12:27:18 -0500

    Week 1 Updates

---
+++ DNAUtility.h
@@ -0,0 +1,81 @@
+//
+// Created by Moad, Karl on 2/19/18.
+//
+
+#ifndef DNALGORITHMS_DNAUTILITY_H
+#define DNALGORITHMS_DNAUTILITY_H
+
+#include <string>
+#include <algorithm>
+
+namespace DNA {
+
+    class DNAUtility {
+    private:
+
+        //Provides the complement to the base provided
+        static char BaseComplement(char base){
+            switch(base){
+                case 'A': return 'T';
+                case 'T': return 'A';
+                case 'C': return 'G';
+                case 'G': return 'C';
+                default: return '\0';
+            }
+        }
+
+    public:
+
+        //Finds the longest common starting substring to which both inputs share
+        static std::string LongestCommonPrefix(std::string &s1, std::string &s2) {
+            long i = 0L;
+
+            while (i < s1.size() && i < s2.size() && s1.at(i) == s2.at(i)) {
+                i++;
+            }
+
+            if (i > 0) {
+                return s1.substr(0, i);
+            } else {
+                return std::string("");
+            }
+        }
+
+        //Informs if the supplied input match or not
+        static bool Match(std::string &s1, std::string &s2) {
+            return s1.compare(s2) == 0;
+        }
+
+        //returns a string complement of the input string
+        static std::string Complement(std::string &s1){
+            std::string out;
+
+            for(long i = 0; i < s1.size(); i++){
+                out += BaseComplement(s1.at(i));
+            }
+
+            return out;
+        }
+
+        //returns a reversed string complement of the input string
+        static std::string ReverseComplement(std::string &s1){
+            std::string complement = Complement(s1);
+            std::reverse(complement.begin(), complement.end());
+
+            return complement;
+        }
+
+        //return the Phred33 ASCII encoded value for the corresponding Q value
+        static char QualityToPhred33(int Q){
+            return (char)(Q + 33);
+        }
+
+        //Return the quality value of the provided Phred33 value
+        static int Phred33toQuality(char P){
+            return (int)P - 33;
+        }
+    };
+};
+
+
+#endif //DNALGORITHMS_DNAUTILITY_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include "DNAUtility.h"
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
 
 int main(int argc, char* argv[]){
 
-}
+    std::string s1("ACCATGGTTA");
+    std::string s2("ACCAGTGTAAA");
+    std::string s3("ACCTTGG");
+    std::string s4 = s3;
 
+
+    std::cout << "Longest Common Prefix  S1 & S2:" << DNA::DNAUtility::LongestCommonPrefix(s1,s2) << std::endl;
+
+    std::cout << "Match S1 & S2: " << (DNA::DNAUtility::Match(s1,s2) ? "True" : "False") << std::endl;
+
+    std::cout << "Match S3 & S4: " << (DNA::DNAUtility::Match(s3,s4) ? "True" : "False") << std::endl;
+
+
+    std::cout << "Complement : " << s3 << " : " << DNA::DNAUtility::Complement(s3) << std::endl;
+    std::cout << "RComplement: " << s3 << " : " << DNA::DNAUtility::ReverseComplement(s3) << std::endl;
+
+
+    std::string fileBuffer;
+    std::string f1("/Users/moadkj/Documents/code/git/DNAlgorithms/data/lambda_virus.fa");
+
+    std::ifstream file(f1); // pass file name as argment
+    std::string linebuffer;
+
+    while (file && getline(file, linebuffer)){
+        if (linebuffer.length() == 0)continue;
+        if(linebuffer.at(0) == '>') continue;  //skip the meta header
+
+        fileBuffer += linebuffer;
+
+    }
+
+    file.close();
+
+    std::cout <<  "Lambda virus DNA: " << fileBuffer << std::endl;
+    std::cout << "Genome size: " << fileBuffer.size() << std::endl;
+
+    std::map<char, long> base_freq = {{'A', 0L}, {'T', 0L}, {'C',0L}, {'G',0L}};
+
+    for(long i = 0; i < fileBuffer.size(); i++){
+        base_freq[fileBuffer.at(i)]++;
+    }
+
+    std::cout << "Base Frequencies:" << std::endl;
+    std::cout << "A : " << base_freq['A'] << std::endl;
+    std::cout << "T : " << base_freq['T'] << std::endl;
+    std::cout << "C : " << base_freq['C'] << std::endl;
+    std::cout << "G : " << base_freq['G'] << std::endl;
+
+    std::cout << "Frequency check :" << ((base_freq['A'] + base_freq['T'] + base_freq['C'] + base_freq['G']) == fileBuffer.size() ? "Positive" : "Negative");
+}
 
 
 
